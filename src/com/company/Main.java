051db86e0d5c77dd0f0d3671054b2f5fbc7feb46
Alexander <89515014507@mail.ru>
2021-04-24 02:57:54 +0600

    14 Tasks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,59 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Git!!!");
-        // контроль версии нужен для совмещения работы над одним проэктом и контроля его изменений
-        // для упрощения данной операци в которой встречаются и разница во временни а так же в растоянии допустим в другой стране
-        // что бы видеть когда и какие изменения были внесенны в проэкт групой человек или их разными индевидами
-        // используется гидхаб
-        // так же гит позволяет вернуть первоночальный код до изменения каким либо программистом если вдруг что то пошло не так
-        // команда в гит хабе Commit как я понимаю это коминтарий формы и вида изменения с указание в каком файле и для ччего это
-        // было сделано
-        // команда push эот заливка измененного проэкта в основной корневой коталог или в ветку которая находится на сервере
-        // по сути гид как я понял это что то наподобии основной виртуальной папки с возможностью отката изменений
-        // при этом откат можно проводить выбарочно
+        Scanner sc = new Scanner(System.in);
+        Student[] man = new Student[3];
+
+        for (int i = 0; i < man.length; i++) {
+            man[i] = new Student();
+            System.out.println("Введите имя студента");
+            man[i].name = sc.nextLine();
+            System.out.println("Введите возрост студента");
+            man[i].age = sc.nextInt();
+            System.out.println("Ввдетие возрост студента");
+            man[i].height = sc.nextInt();
+            System.out.println("Введите на какую професию учится студетн");
+            man[i].professia = sc.nextLine();
+            System.out.println("Является ли студент отличником");
+            man[i].excellStudy = sc.nextBoolean();
+        }
+
+        for (Student student : man) {
+            System.out.println("Имя Студента" + student.name);
+            System.out.println("Возрост студента" + student.age);
+            if (student.age < 16) {
+                System.out.println("Не может быть студентом так как учится в школе!!!");
+            }
+            if (student.age > 30) {
+                System.out.println("Не может быть студентом так как он работает");
+            } else {
+                System.out.println("Является студентом!!");
+            }
+            System.out.println("Рост студента" + student.height);
+            if (student.height > 175) {
+                System.out.println("Высокого роста");
+            }
+            if (student.height < 150) {
+                System.out.println("Студент является коротышкой!!!");
+            } else {
+                System.out.println(" Нормальный рост студента");
+            }
+            System.out.println("Професию которорую выбрал студент");
+            if (student.excellStudy) {
+                System.out.println("Является отличником");
+            }
+        }
     }
+}
+
+class Student {
+    String name;
+    int age;
+    int height;
+    String professia;
+    boolean excellStudy;
 }
