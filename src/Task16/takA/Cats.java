1a42b0d674b61b8cae0a702418645320fa1eb536
Alexander <89515014507@mail.ru>
2021-05-01 02:58:10 +0600

    beta TaskA

---
+++ src/Task16/takA/Cats.java
@@ -0,0 +1,29 @@
+package Task16.takA;
+
+import java.util.Scanner;
+
+public class Cats {
+    String name;
+    int age;
+    boolean home;
+
+    public void setAge(int age){
+        Scanner sc = new Scanner(System.in);
+        if(age <= 0){
+            System.err.println("Нельзя задать отрицательное число задайте число больше нуля");
+            age = sc.nextInt();
+            setAge(age);
+        }else{ this.age = age;}
+    }public int getAge(){return age;}
+
+    public void setName(String name){
+        this.name = name;
+    }public String getName(){return name;}
+
+    public void setHome(boolean home){
+        this.home = home;
+    }public boolean getHome(){return home;}
+
+
+
+}
--- src/Task16/takA/TaskA.java
+++ src/Task16/takA/TaskA.java
@@ -6,61 +6,16 @@ import java.util.Random;
 public class TaskA {
     public static void main(String[] args) {
         Random ran = new Random();
-//        Scanner sc = new Scanner(System.in);
-        Bird[] perro = new Bird[3];
-        Cat[] cats = new Cat[3];
-        Dog[] dogs = new Dog[3];
-        Fish[] fishs = new Fish[3];
-
-        for (int i = 0; i < cats.length; i++) {
-            perro[i] = new Bird();
-            cats[i] = new Cat();
-            dogs[i] = new Dog();
-            fishs[i] = new Fish();
-
-            fishs[i].setName(getRandomNames());
-            cats[i].setName(getRandomNames());
-            dogs[i].setName(getRandomNames());
-            perro[i].setName(getRandomNames());
-
-            perro[i].setWeight(ran.nextInt(100 - 1 + 1) + 1);
-            dogs[i].setWeight(ran.nextInt(100 - 1 + 1) + 1);
-            cats[i].setWeight(ran.nextInt(100 - 1 + 1) + 1);
-            fishs[i].setWeight(ran.nextInt(100 - 1 + 1) + 1);
-
-            perro[i].setHeight(ran.nextInt(100 - 1 + 1) + 1);
-            dogs[i].setHeight(ran.nextInt(100 - 1 + 1) + 1);
-            cats[i].setHeight(ran.nextInt(100 - 1 + 1) + 1);
-            fishs[i].setHeight(ran.nextInt(100 - 1 + 1) + 1);
-
-            perro[i].setAge(ran.nextInt(100 - 1 + 1) + 1);
-            dogs[i].setAge(ran.nextInt(100 - 1 + 1) + 1);
-            cats[i].setAge(ran.nextInt(100 - 1 + 1) + 1);
-            fishs[i].setAge(ran.nextInt(100 - 1 + 1) + 1);
-
-            // Лень было делать постоянную пропись со скана так как очень долго это все происходит
-            // вот закомитил как он пишется прошу строго не судить
-            // Хочу чтобы код сам себя отрабатывал так как на все классы в масивах инфу прописывать реально лень.
-//            perro[i].setAge(sc.nextInt());
-//            dogs[i].setAge(sc.nextInt());
-//            cats[i].setAge(sc.nextInt());
-//            fishs[i].setAge(sc.nextInt());
-
-            perro[i].setHome(ran.nextBoolean());
-            dogs[i].setHome(ran.nextBoolean());
-            cats[i].setHome(ran.nextBoolean());
-            fishs[i].setHome(ran.nextBoolean());
 
 
 
-
-
-
-        }
     }
+
     static String getRandomNames(){
+        // создал генератор случайного имени (Бред конечно но ине было интересно как это реализовано
+
         Random ran = new Random();
-        int num = ran.nextInt(10 - 4 + 1) + 1;
+        int num = ran.nextInt(10 - 4 + 1) + 4;
         StringBuilder names = new StringBuilder();
         int numWord;
         for(int i = 0; i < num;i++){
