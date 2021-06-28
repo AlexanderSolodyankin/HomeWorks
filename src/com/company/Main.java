5d9ce742218070e00f6ce6431786de18c76854c4
AlexanderSolodyankin <89515014507@mail.ru>
2021-06-28 22:01:51 +0600

    Первая задачка из третьего экзамена

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,25 @@
 package com.company;
 
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
-        /**
-         * Пример как создовать случайные имена
-         */
-//            RandomName name = SetupName.getName(Gender.MALES);
-//            String man = name.getName();
+        Set<Integer> arrayInteger = new LinkedHashSet<>();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            int num = arrayInteger.size();
+            if(i % 5 == 0){arrayList.add(0,i);
+            }else { arrayList.add(arrayList.size(),i); }
+        }
+        arrayInteger.addAll(arrayList);
+        System.out.println(arrayInteger);
+
+
 
     }
 }
