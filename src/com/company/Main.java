6385d6305cf2d1012d6eb3ad2b09262112d8ad7a
AlexanderSolodyankin <89515014507@mail.ru>
2021-06-29 03:18:51 +0600

    Экзаменационая работа задача вторая: Доработка

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,34 +2,34 @@ package com.company;
 
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        LinkedList<Person> arrayPerson = new LinkedList<>();
-        Person person = new Person("da","da","da",true);
-        Person person1 = new Person("da","da","da",true);
-        arrayPerson.add(person);
+//        LinkedList<Person> arrayPerson = new LinkedList<>();
+//        Person person = new Person("da","da","da",true);
+//        Person person1 = new Person("da7","da","da",true);
+//        arrayPerson.add(person);
+//
+//        arrayPerson = attachObject( arrayPerson, person1);
 
+        System.out.println();
+    }
+    public static LinkedList<Person> attachObject(LinkedList<Person> arrayObject, Person object){
         try {
-            if (arrayPerson.contains(person1)) {
+            if(arrayObject.contains(object)){
                 throw new RuntimeException();
-
-            } else {
-                arrayPerson.add(person1);
             }
-            System.out.println(arrayPerson.size());
-        }catch (RuntimeException exp){
-            System.err.println("Попался похожий обьект и я вывел ошибку");
+            else {
+                arrayObject.add(object);
+            }
+        }catch (RuntimeException ranExept){
+            System.err.println("Попался обьект который находится в массиве");
+        }finally {
+            System.out.println("Длина массива: " + arrayObject.size());
         }
-        finally {
-            System.out.println("Длина масива на которой произошла ошибка " + arrayPerson.size());
-        }
-
-        // описана основна суть домашки если успею сделаю более интереснее
-
-
-
+        return arrayObject;
 
     }
 
