e93a506893d8833e92fa722b412bb6030175129c
Alexander <89515014507@mail.ru>
2021-08-10 20:17:46 +0600

    Lessons 51

--- src/main/org/example/Main.java
+++ src/main/org/example/Main.java
@@ -1,16 +1,27 @@
 package org.example;
 
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss yyyy/MM/dd");
-        Calendar cal = Calendar.getInstance();
-        System.out.println(dateFormat.format(cal.getTime()));
+            Map<Integer, Integer> numberCount = new HashMap<>();
+        Random ran = new Random();
+
+        for (int i = 0; i < 1000; i++) {
+            Integer currentNumber = ran.nextInt(101);
+            if(numberCount.containsKey(currentNumber)){
+                numberCount.put(currentNumber, numberCount.get(currentNumber) + 1);
+            }else {
+                numberCount.put(currentNumber, 1);
+            }
+        }
+
+        for(Map.Entry<Integer, Integer> enty : numberCount.entrySet()){
+            System.out.println(enty);
+        }
 
     }
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,24 +1,10 @@
-<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@page pageEncoding="UTF-8"%>
 
 <html>
 <body>
-<h2 style="text-align: center;"> Домашняя работа номер 43 !!!!</h2>
+<h2 style="text-align: center;"> Пустая форма  !!!!</h2>
 
-<div style="text-align: center;">
-<form action="${pageContext.request.contextPath}/userset" method="post">
 
-    <input type="text" name="login" value="Введите логин">
-    <br>
-    <input type="password" name="password" value="Введите пароль">
-    <br>
-    <input type="email" name="email" value="Введите почту">
-    <br>
-    <input type="radio" name="gender" value="Man"> Мужчина
-    <input type="radio" name="gender" value="Woman"> Женщина
-    <br>
-    <input type="submit" value="Внести информацию в базу данных">
-</form>
-</div>
 
 </body>
 </html>
