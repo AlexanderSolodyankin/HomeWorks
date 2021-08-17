b49a9fb8ace70e440228cd96174070b13880b520
Alexander <89515014507@mail.ru>
2021-08-17 17:54:53 +0600

    Home Works 46 Start

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,31 @@
 package com.company;
 
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // добавден и упрощен конект к SQL
 
+        Day day = new Day("Понедельник", 15);
+        Day day1 = new Day("Вторник", 15);
+
+        Map<Day, Integer> map = new HashMap<>();
+        map.put(day,10);
+        map.put(day1, 10);
+
+        System.out.println(map);
+        System.out.println(map.get(day));
+        System.out.println(map.get(day1));
 
 
     }
+
+
+
+
+
 }
 
