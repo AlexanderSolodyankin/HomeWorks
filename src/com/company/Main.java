b4784729cf7abaf7b34c67d3caea06e9f56934a7
Alexander <89515014507@mail.ru>
2021-09-18 23:12:20 +0600

    Home works62 complite

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,53 @@
 package com.company;
 
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-
 public class Main {
 
     public static void main(String[] args) {
-        HomeList<Integer> nums = new HomeList<>();
-        nums.addElement(45);
-        nums.addElement(42);
-        nums.addElement(96);
-        nums.addElement(67);
-        System.out.println(nums.getLastElement());
-        System.out.println(nums.getIndexElement(67));
+/**
+ * -----------------Task A ----------------------------
+ */
+        Factorial fact1 = new Factorial() {
+            @Override
+            public int getNumber(int a) {
+                if (a != 1) a = a * getNumber(a - 1);
+                return a;
 
+        /**
+         * Хотел сделать рекурсию при помощи лямбда функции. Так оно не работает или я где-то неправильно сделал?
+         */
+            }
+        };
+        System.out.println(fact1.getNumber(10));
 
+        Factorial fact2 = (n) -> {
+            int result = 1;
+            for (int i = 1; i <= n ; i++) {
+                result = result * i;
+            }
+            return result;
+        };
 
+        System.out.println(fact2.getNumber(10));
+        /**
+         * -----------------Task B ----------------------------
+         */
+
+            RootPow root = (p,a) ->{
+                double result = 0;
+                for (int i = 1; i < (a / 2); i++) {
+                    result = i;
+                    for (int j = 0; j <= p; j++) {
+                        result = result * i;
+                        if(result == a){
+                            System.out.println("Корень в степерни " + p + " от " + a + " = " + i);
+                            return i;
+                        }
+                    }
+                }
+                return result;
+            };
+
+        System.out.println(root.getRootPow(4,81));
     }
 }
---
+++ src/com/company/RootPow.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public interface RootPow {
+
+    double getRootPow(double pow, double answer);
+}
